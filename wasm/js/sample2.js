95e3ed00114dc6cc1346357141685e3edebec397
mith-mmk <47620686+mith-mmk@users.noreply.github.com>
2022-02-16 19:07:47 +0900

    grayscale demo

---
+++ wasm/js/sample2.js
@@ -0,0 +1,86 @@
+import init,{Universe} from "../pkg/grayscale.js";  // Universeは要インポート wasm.Universeでは動かない
+
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+const canvas2 = document.getElementById('canvas2');
+const ctx2 = canvas2.getContext('2d');
+const scale_type = document.getElementById('scale-type');
+const width = canvas.width;
+const height =canvas.height;
+canvas2.width = width;
+canvas2.height = height;
+
+function jsinit(wasm) {
+    const memory = wasm.memory; // 共有メモリーに必要
+    // 共有メモリーのアドレスの取得
+    const universe = Universe.new(width,height);
+    let buffersize = width * height * 4;
+    let ibuf = new Uint8Array(memory.buffer,universe.input_buffer(), buffersize);
+    let buf = new Uint8ClampedArray(memory.buffer,universe.output_buffer(), buffersize);
+    clearCanvas();
+
+    function clearCanvas() {
+      universe.clear(0x0);
+      let imgData = new ImageData(buf, width, height);
+      ctx.putImageData(imgData, 0, 0);
+    }
+
+
+    function toGrayscale() {
+      universe.to_grayscale(scale_type.value);
+      let imgData = new ImageData(buf, width, height);
+      ctx2.putImageData(imgData, 0, 0);
+    }
+
+
+    scale_type.addEventListener('change', (ev) => {
+      toGrayscale();
+    });
+
+    // Drag and Drop
+    canvas.addEventListener('dragover', (ev) => {
+        ev.stopPropagation();
+        ev.preventDefault();
+        canvas.style.border = 'solid 10px #e1e7f0';
+    }, false);
+
+    canvas.addEventListener('drop', (ev) => {
+      ev.stopPropagation();
+      ev.preventDefault();
+      canvas.style.border = '';
+      const files = ev.dataTransfer.files; 
+      if (!files[0].type.match(/image\/*/)) {
+        return;
+      }
+      if (files.length > 1) return alert('Illigal Operation.Multi Files Select.');
+      const reader = new FileReader();
+      const img = new Image();
+      reader.onload = (event) => {
+        img.src = event.target.result;
+        img.onload = (ev) => {
+
+          // スケーリング（画像をCanvasサイズに縮小する）
+          let drawWidth = img.width;
+          let drawHeight = img.height;
+          if (img.width > width) {
+            drawWidth = width;
+            drawHeight = img.height / img.width * height;
+          }
+          if (drawHeight > height){
+            drawHeight = height;
+            drawWidth = img.width / img.height * drawWidth;
+          }
+          clearCanvas();
+          ctx.drawImage(img,0,0,img.width,img.height,0,0,drawWidth,drawHeight);
+          const imgData = ctx.getImageData(0, 0, width, height);
+          ibuf.set(imgData.data);
+          toGrayscale();
+        }
+      };
+      reader.readAsDataURL(files[0]);
+    }, false);
+}
+
+init().then((wasm) => {
+  jsinit(wasm);
+});
---
+++ wasm/pkg/grayscale.js
@@ -0,0 +1,158 @@
+
+let wasm;
+
+let cachedTextDecoder = new TextDecoder('utf-8', { ignoreBOM: true, fatal: true });
+
+cachedTextDecoder.decode();
+
+let cachegetUint8Memory0 = null;
+function getUint8Memory0() {
+    if (cachegetUint8Memory0 === null || cachegetUint8Memory0.buffer !== wasm.memory.buffer) {
+        cachegetUint8Memory0 = new Uint8Array(wasm.memory.buffer);
+    }
+    return cachegetUint8Memory0;
+}
+
+function getStringFromWasm0(ptr, len) {
+    return cachedTextDecoder.decode(getUint8Memory0().subarray(ptr, ptr + len));
+}
+/**
+*/
+export class Universe {
+
+    static __wrap(ptr) {
+        const obj = Object.create(Universe.prototype);
+        obj.ptr = ptr;
+
+        return obj;
+    }
+
+    __destroy_into_raw() {
+        const ptr = this.ptr;
+        this.ptr = 0;
+
+        return ptr;
+    }
+
+    free() {
+        const ptr = this.__destroy_into_raw();
+        wasm.__wbg_universe_free(ptr);
+    }
+    /**
+    * @param {number} width
+    * @param {number} height
+    * @returns {Universe}
+    */
+    static new(width, height) {
+        var ret = wasm.universe_new(width, height);
+        return Universe.__wrap(ret);
+    }
+    /**
+    * @returns {number}
+    */
+    width() {
+        var ret = wasm.universe_width(this.ptr);
+        return ret >>> 0;
+    }
+    /**
+    * @returns {number}
+    */
+    height() {
+        var ret = wasm.universe_height(this.ptr);
+        return ret >>> 0;
+    }
+    /**
+    * @returns {number}
+    */
+    input_buffer() {
+        var ret = wasm.universe_input_buffer(this.ptr);
+        return ret;
+    }
+    /**
+    * @returns {number}
+    */
+    output_buffer() {
+        var ret = wasm.universe_output_buffer(this.ptr);
+        return ret;
+    }
+    /**
+    * @param {number} color
+    */
+    clear(color) {
+        wasm.universe_clear(this.ptr, color);
+    }
+    /**
+    * @param {number} startx
+    * @param {number} starty
+    * @param {number} width
+    * @param {number} height
+    * @param {number} color
+    */
+    fillrect(startx, starty, width, height, color) {
+        wasm.universe_fillrect(this.ptr, startx, starty, width, height, color);
+    }
+    /**
+    * @param {number} t
+    */
+    to_grayscale(t) {
+        wasm.universe_to_grayscale(this.ptr, t);
+    }
+}
+
+async function load(module, imports) {
+    if (typeof Response === 'function' && module instanceof Response) {
+        if (typeof WebAssembly.instantiateStreaming === 'function') {
+            try {
+                return await WebAssembly.instantiateStreaming(module, imports);
+
+            } catch (e) {
+                if (module.headers.get('Content-Type') != 'application/wasm') {
+                    console.warn("`WebAssembly.instantiateStreaming` failed because your server does not serve wasm with `application/wasm` MIME type. Falling back to `WebAssembly.instantiate` which is slower. Original error:\n", e);
+
+                } else {
+                    throw e;
+                }
+            }
+        }
+
+        const bytes = await module.arrayBuffer();
+        return await WebAssembly.instantiate(bytes, imports);
+
+    } else {
+        const instance = await WebAssembly.instantiate(module, imports);
+
+        if (instance instanceof WebAssembly.Instance) {
+            return { instance, module };
+
+        } else {
+            return instance;
+        }
+    }
+}
+
+async function init(input) {
+    if (typeof input === 'undefined') {
+        input = new URL('grayscale_bg.wasm', import.meta.url);
+    }
+    const imports = {};
+    imports.wbg = {};
+    imports.wbg.__wbindgen_throw = function(arg0, arg1) {
+        throw new Error(getStringFromWasm0(arg0, arg1));
+    };
+
+    if (typeof input === 'string' || (typeof Request === 'function' && input instanceof Request) || (typeof URL === 'function' && input instanceof URL)) {
+        input = fetch(input);
+    }
+
+
+
+    const { instance, module } = await load(await input, imports);
+
+    wasm = instance.exports;
+    init.__wbindgen_wasm_module = module;
+
+    return wasm;
+}
+
+export default init;
+
