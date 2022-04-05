dd5f45e0c31d499d4d37ca9c23680525debd9b2d
mith-mmk <47620686+mith-mmk@users.noreply.github.com>
2022-04-05 19:35:32 +0900

    alert

---
+++ wasm/js/load-affine.js
@@ -0,0 +1,10 @@
+let ua = navigator.userAgent;
+
+if (! (ua.match(/Android/)  && ua.match(/Chrome/)) ) {
+    import("./affine.js");
+} else {
+    let flag = window.confirm('Android Chromeで一部環境ではアプリが固まるする可能性があります。リスクを承知して良ければOKを押してください');
+    if (flag) {
+        import("./affine.js");
+    }    
+}
